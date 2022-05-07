d91339fa12e06a19f16476a3300e20564226d082
Marina <kovalenko_marina@inbox.ru>
2022-05-07 19:57:25 +0700

    наибольшее из трех

---
+++ zadacha3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите числa ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (a>b & a > c);
+{
+    max = a;
+}
+if (c > a & c > b);
+{
+    max = c;
+}
+if (b>c & b>a);
+{
+    max = b;
+}
+Console.Write("Наибольшее число = ");
+Console.WriteLine(max);
