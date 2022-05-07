41e32aed504e056ca7a3be94ba94174432ff2c84
Marina <kovalenko_marina@inbox.ru>
2022-05-07 20:28:57 +0700

    последняя цифра с проверкой трехзначности

--- zadacha5/Program.cs
+++ zadacha5/Program.cs
@@ -1,4 +1,11 @@
 ﻿Console.WriteLine("Введите число ");
 int n = int.Parse(Console.ReadLine());
-Console.WriteLine("Последняя цифра ");
-Console.Write(n%10);
+if (n>99 && n<1000)
+{
+    Console.WriteLine("Последняя цифра ");
+    Console.Write(n%10);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
