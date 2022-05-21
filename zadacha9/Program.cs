762d10fc9ae4451643069296cf78792d18735464
Marina <kovalenko_marina@inbox.ru>
2022-05-21 14:14:14 +0700

    произведение чисел

---
+++ zadacha9/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение чисел от 1 до {a} равна ");
+int n = 1;
+int work = 1;
+while (n <= a)
+{
+    work= work*n;    
+    n++;
+}
+Console.Write(work);
