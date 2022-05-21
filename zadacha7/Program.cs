e0e30d00f6938b747150a35eaa185c571760ae8e
Marina <kovalenko_marina@inbox.ru>
2022-05-21 12:38:01 +0700

    список квадратов

---
+++ zadacha7/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Список квадратов чисел от 1 до {n} ");
+int a = 1;
+while (a <= n)
+{
+    int b = a*a;
+    Console.Write(b + " ");
+    a++;
+}
