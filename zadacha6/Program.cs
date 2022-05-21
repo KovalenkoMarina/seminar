30d1eaa87ca054b24d916d9ca2a7c4dc296f4f0b
Marina <kovalenko_marina@inbox.ru>
2022-05-21 13:54:58 +0700

    сумма чисел

---
+++ zadacha6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма чисел от 1 до {a} равна ");
+int n = 1;
+int sum = 0;
+while (n <= a)
+{
+    sum= sum + n;    
+    n++;
+}
+Console.Write(sum);
