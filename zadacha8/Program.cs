25ddcbb3dc81d80f3b0b219ea9d4b6aef2d032c2
Marina <kovalenko_marina@inbox.ru>
2022-05-21 14:07:01 +0700

    подсчет цифр в числе

---
+++ zadacha8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine());
+int count = 0;
+while (n > 0)
+{
+    n = n/10;
+    count++;
+}
+Console.WriteLine($"Цифр в числе {count}");
+
