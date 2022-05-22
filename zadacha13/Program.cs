4684999742b07841d628fe5920e9ffaf36422f48
Marina <kovalenko_marina@inbox.ru>
2022-05-22 15:25:37 +0700

    сложение нечетных элеметов массива

---
+++ zadacha13/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {3, 4, 5, 2, 1, 6};
+int count = array.Length;
+int sum = 0;
+for (int i = 0; i<count; i++)
+    {
+        if (i%2==1)
+        {
+            sum = sum + array[i];
+            
+        }
+    }
+Console.Write(sum);
+
