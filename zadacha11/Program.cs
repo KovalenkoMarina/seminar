51199a852f374aa289e2f878ca79b13c8fdb4d76
Marina <kovalenko_marina@inbox.ru>
2022-05-22 14:50:13 +0700

    массив замена элементов на противоположные

---
+++ zadacha11/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = {3, 4, 5, 2, 1, 6};
+void chang(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i<count; i++)
+    {
+        array[i]= - array[i];
+    }
+}
+void Printchang(int[] array)
+{
+    int count = array.Length;
+    for (int j = 0; j<count; j++)
+    {
+        Console.WriteLine($"{array[j]}");
+    }
+}
+chang(array);
+Printchang(array);
