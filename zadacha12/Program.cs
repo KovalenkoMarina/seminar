c386cc8719d9002bafaca142ed6b716f0c300e5a
Marina <kovalenko_marina@inbox.ru>
2022-05-22 15:14:12 +0700

    поиск элемента с определенным значением в массиве

---
+++ zadacha12/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {3, 4, 5, 2, 1, 6};
+int count = array.Length;
+int a = 0;
+for (int i = 0; i<count; i++)
+    {
+        if (array[i]== 3)
+        {
+            a++;
+            break;
+        }
+    }
+if (a !=0) Console.Write("yes");
+else  Console.Write("no");
