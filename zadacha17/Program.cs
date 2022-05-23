75b309f87eb74b1b7d3dfb0527387e9953b66fd7
Marina <kovalenko_marina@inbox.ru>
2022-05-23 16:08:59 +0700

    числа фибоначи

---
+++ zadacha17/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать числа Фибоначчи меньше заданного числа N
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()?? "");
+
+int firstValue  = 0;
+int secondValue = 1;
+int result      = 0;
+Console.Write($"{firstValue} {secondValue} ");
+
+while(result > N)
+{
+    result = firstValue + secondValue;
+    if(result > N) break;
+    Console.Write(result + " ");
+    firstValue = secondValue;
+    secondValue = result;
+}
+
